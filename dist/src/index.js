0a8116a5b709e8cf3d27ca76cdaf49636b9b31f9
FM-Akib <muntasirakib08@gmail.com>
2024-11-07 12:19:58 +0600

    first commit

---
+++ dist/src/index.js
@@ -0,0 +1,25 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import db from './db/db.js';
+import { usersTable } from './db/schema.js';
+const app = new Hono();
+app.get('/', (c) => {
+    return c.text('Hello Hono!');
+});
+app.get('/users', async (c) => {
+    try {
+        const users = await db.select().from(usersTable);
+        return c.json(users);
+    }
+    catch (error) {
+        console.error('Database error:', error);
+        return c.json({ error: 'Failed to fetch users' }, 500);
+    }
+});
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+serve({
+    fetch: app.fetch,
+    port
+});
+export default app;
